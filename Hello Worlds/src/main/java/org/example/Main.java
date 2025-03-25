10a57cc99d476b6ff6b0064bcb7cebb29fda2318
Jeong Min Hyeok <wjdalsgur1234@cju.ac.kr>
2025-03-25 11:39:56 +0900

    2021011548 정민혁

---
+++ Hello Worlds/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+ <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을
+public class Main {
+    public static void main(String[] args) {
+        for(String arg: args) {
+            System.out.println(arg);//TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+        }
+    }
+}
