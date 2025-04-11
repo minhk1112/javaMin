1d648e9238967980e5477aecc04a1dcecb1bd3c8
Jeong Min Hyeok <wjdalsgur1234@cju.ac.kr>
2025-04-11 13:35:04 +0900

    20250411 Practice

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tired;
+
+        System.out.println("피로도를 입력해주세요 : ");
+        tired = sc.nextInt();
+
+        if (tired < 90) {
+            System.out.println("운동을 가야합니다.");
+        }
+        else {
+            System.out.println("운동을 쉬어야 합니다.");
+        }
+    }
+}
