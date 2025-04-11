9b0b94f514d7c34a38c288d4fc4d58fc961c2c35
Jeong Min Hyeok <wjdalsgur1234@cju.ac.kr>
2025-04-11 13:38:51 +0900

    20250411 Practice

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int tired;
+        int tired = 0;
 
         System.out.println("피로도를 입력해주세요 : ");
         tired = sc.nextInt();
