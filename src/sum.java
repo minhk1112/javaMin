beb12484893a4773560c06459a9c719d564b6c26
Jeong Min Hyeok <wjdalsgur1234@cju.ac.kr>
2025-05-13 11:03:22 +0900

    20250513

---
+++ src/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i = 1; i <= 1000; i++){
+            if(i%2==0){
+                sum+=i;
+            }
+        }
+        System.out.println("2부터 1000까지 짝수의 합: " + sum);
+    }
+}
